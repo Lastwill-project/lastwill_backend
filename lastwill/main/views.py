2e008e39e6a39ad1174c824fae0de39173bc2dbd
aleksei-udalov <aleksei.udalov.01@gmail.com>
2017-08-09 21:32:49 +0400

    contract template

--- lastwill/main/views.py
+++ lastwill/main/views.py
@@ -1,12 +1,13 @@
 import requests
 import json
+import re
 from django.http import JsonResponse
 from django.shortcuts import render_to_response
 from django.middleware import csrf
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import APIException
 from lastwill.settings import ETHERSCAN_API_URL, API_KEY
-from lastwill import check
+from lastwill import check, settings
 
 def index(request):
     csrf_token = csrf.get_token(request)
@@ -40,6 +41,12 @@ def create_contract(request):
             raise APIException(code=400, detail='percent %s is not valid' % heir['percent'])
     if sum([x['percent'] for x in request.data['heirs']]) != 100:
         raise APIException(code=400, detail='percents sum is not equal to 100')
-    # generate contract
+    with open('../lastwill/contracts/LastWillContractTemplate.sol', 'r') as contract_file:
+        contract = contract_file.read()
+        contract = re.sub('{{targetUser}}', request.data['address'], contract)
+        contract = re.sub('{{lastWillAccount}}', settings.LASTWILL_ACCOUNT, contract)
+        contract = re.sub('{{recipientPercents}}', 'zzz', contract)
+
+    print(contract) 
     # deploy
-    return JsonResponse({})
+    return JsonResponse({'contract': contract})
