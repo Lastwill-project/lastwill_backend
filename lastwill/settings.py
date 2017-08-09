caab6f5412227be0b50fbadeca434453aca70a93
aleksei-udalov <a@a.com>
2017-08-09 15:05:19 +0300

    settings fix

--- lastwill/settings.py
+++ lastwill/settings.py
@@ -134,3 +134,10 @@ STATICFILES_DIRS = (
 
 ETHERSCAN_API_URL = 'https://api.etherscan.io/api'
 API_KEY = 'G9G5NYXAFKJZ7R9YPKA9FQHCKZGX5HZZUB'
+
+try:
+    from students.settings_local import *
+except ImportError as exc:
+    __import__('warnings').warn("Can't load local settings: {}".format(st$
+        
+        
